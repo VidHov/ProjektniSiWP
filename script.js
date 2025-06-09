10a5158e6a5afa7ad5421bd3225ef01d9c8bd726
VidHov <vidhovorka@gmail.com>
2025-06-09 18:18:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
             form.reset();
         });
     }
+    
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetElement = document.querySelector(this.getAttribute('href'));
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
